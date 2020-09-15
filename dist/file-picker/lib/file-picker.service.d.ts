dadc2b0f144809f68f7eebd84dc32e3c5f397746
Anastasia Chechulina <anastasia.chechulina87@gmail.com>
2020-09-15 17:15:28 +0200

    removing dist

--- dist/file-picker/lib/file-picker.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from 'rxjs';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-export declare class FilePickerService {
-    private sanitizer;
-    constructor(sanitizer: DomSanitizer);
-    mockUploadFile(formData: any): Observable<any>;
-    createSafeUrl(file: any): SafeResourceUrl;
-}
--- dist/file-picker/lib/file-upload.utils.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function getFileType(fileExtension: string): string;
